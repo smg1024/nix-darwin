8fb556739da00d258db8680b1f3c25ed49a76a61
Poby <87608318+smg1024@users.noreply.github.com>
2026-07-31 10:23:56 +0900

    feat: add huginn specific ssh config to homeModules

--- modules/aspects/huginn.nix
+++ modules/aspects/huginn.nix
@@ -1,4 +1,18 @@
 {...}: {
+  repo.homeModules.huginn = {config, ...}: {
+    programs.ssh = {
+      settings = {
+        "kmeat-ai-workstation" = {
+          Hostname = "192.9.1.40";
+          User = "kmeatai";
+          Port = 10222;
+          IdentitiesOnly = true;
+          IdentityFile = [config.sops.secrets."github_ssh_key".path];
+        };
+      };
+    };
+  };
+
   flake.modules.darwin.huginn = {
     homebrew = {
       brews = [
